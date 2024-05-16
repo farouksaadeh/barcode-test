7d5395e07d78c4dd9af8caaf7d3ac78aa98bd536
Farouk Saadeh <farouk.saadeh@lernende.bbw.ch>
2024-05-16 10:56:45 +0200

    changed url

--- src/Fabio/italienisch.js
+++ src/Fabio/italienisch.js
@@ -8,31 +8,31 @@ const Italienisch = () => {
         "name": "Lasagne",
         "description": "Ein traditionelles italienisches Gericht, bestehend aus geschichteten Nudeln, Hackfleischsauce und Käse.",
         "image": "https://www.myfoodom.com/wp-content/uploads/2020/03/Copy-of-Untitled-10.jpg",
-        "qrCode": "https://barcode-test-tan.vercel.app//italienisch/lasagne"
+        "qrCode": "https://barcode-test-ochre.vercel.app//italienisch/lasagne"
       },
       {
         "name": "Pizza Margherita",
         "description": "Eine traditionelle italienische Pizza mit Tomatensauce, Mozzarella und Basilikum.",
         "image": "https://th.bing.com/th/id/R.9e0caa8628009f5bb525d1e09604fda9?rik=dI0nOfBy3ahp1w&riu=http%3a%2f%2fvitaitaliantours.com%2fwp-content%2fuploads%2f2016%2f03%2fNeapolitan-Pizza-Margherita.jpg&ehk=Rk%2bfgzpAHbGig6ZHOExH6bwW62dG29BR4XPCsHqKBSI%3d&risl=1&pid=ImgRaw&r=0",
-        "qrCode": "https://barcode-test-tan.vercel.app//italienisch/pizza-margherita"
+        "qrCode": "https://barcode-test-ochre.vercel.app//italienisch/pizza-margherita"
       },
       {
         "name": "Risotto",
         "description": "Ein cremiges Reisgericht aus Italien, das je nach Variation verschiedene Zutaten wie Pilze, Gemüse oder Meeresfrüchte enthalten kann.",
         "image": "https://d1bvpoagx8hqbg.cloudfront.net/originals/recette-risotto-aux-champignons-afd1003257c94937589cf51305918abe.jpg",
-        "qrCode": "https://barcode-test-tan.vercel.app//italienisch/risotto"
+        "qrCode": "https://barcode-test-ochre.vercel.app//italienisch/risotto"
       },
       {
         "name": "Spaghetti Carbonara",
         "description": "Ein klassisches italienisches Gericht mit Spaghetti, Ei, Guanciale und Pecorino Romano.",
         "image": "https://cdn-cas.orami.co.id/parenting/original_images/easy_spaghetti_carbonara.jpg",
-        "qrCode": "https://barcode-test-tan.vercel.app//italienisch/spaghetti-carbonara"
+        "qrCode": "https://barcode-test-ochre.vercel.app//italienisch/spaghetti-carbonara"
       },
       {
         "name": "Tiramisu",
         "description": "Ein klassisches italienisches Dessert aus Löffelbiskuits, Mascarpone, Kaffee und Kakao.",
         "image": "https://www.simplyrecipes.com/thmb/n29cy_Z8pl8r8Au-MpA1Lp08kdg=/2000x1333/filters:fill(auto,1)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2017__10__2017-10-28-Tiramisu-21-7de4673191d542039f47927fde4fee82.jpg",
-        "qrCode": "https://barcode-test-tan.vercel.app//italienisch/tiramisu"
+        "qrCode": "https://barcode-test-ochre.vercel.app//italienisch/tiramisu"
       }
     
     
--- src/Nemanja/serbisch.js
+++ src/Nemanja/serbisch.js
@@ -8,61 +8,61 @@ const Serbisch = () => {
       name: "Ćevapi",
       description: "Ćevapi sind traditionelle serbische Würstchen aus gemischtem Hackfleisch, oft mit Zwiebeln und Paprika serviert.",
       image: "https://th.bing.com/th/id/R.0ea90b2486aa803a2f07976c933dfa68?rik=bbtnwyzMn6UHxA&pid=ImgRaw&r=0",
-      qrCode: "https://barcode-test-tan.vercel.app//serbisch/cevapi"
+      qrCode: "https://barcode-test-ochre.vercel.app//serbisch/cevapi"
     },
     {
       name: "Pljeskavica",
       description: "Pljeskavica ist ein serbisches Burger-Patty aus gemahlenem Fleisch, oft serviert mit Kajmak, Zwiebeln und Ajvar in einem Brot.",
       image: "https://lh3.googleusercontent.com/ET8qj9I6Y7h1OPUCPwbNyR9jKjTmXGHu4YslyYQgCBlrDiYV54uN1xqGrIrvzTaKDmtc0RgdCZFwXDaLlbpBfPDn3Ru4L_4VdU1Otg=w900-l68",
-      qrCode: "https://barcode-test-tan.vercel.app//serbisch/pljeskavica"
+      qrCode: "https://barcode-test-ochre.vercel.app//serbisch/pljeskavica"
     },
     {
       name: "Gibanica",
       description: "Gibanica ist ein serbischer Käsekuchen aus Schichten von Filoteig und Käse, oft mit Eiern und Sauerrahm vermischt.",
       image: "https://www.thespruceeats.com/thmb/3ejDDtStAsWvep_poJa12TmkdV0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/SES-salty-serbian-gibanica-1135905-hero-8baa62be52ec49d9b9dcf0d7fb889b9a.jpg",
-      qrCode: "https://barcode-test-tan.vercel.app//serbisch/gibanica"
+      qrCode: "https://barcode-test-ochre.vercel.app//serbisch/gibanica"
     },
     {
       name: "Ajvar",
       description: "Ajvar ist eine serbische Paprika-Paste aus gerösteten Paprika, Knoblauch, Auberginen und Gewürzen.",
       image: "https://th.bing.com/th/id/OIP.KocmXSwZBHc_ocCWQB8DsgHaFj?rs=1&pid=ImgDetMain",
-      qrCode: "https://barcode-test-tan.vercel.app//serbisch/ajvar"
+      qrCode: "https://barcode-test-ochre.vercel.app//serbisch/ajvar"
     },
     {
       name: "Sarma",
       description: "Sarma sind serbische Kohlrouladen, gefüllt mit Hackfleisch, Reis und Gewürzen, oft in einer Tomatensoße gekocht.",
       image: "https://cms-receitadevovo-gallery.s3.us-east-2.amazonaws.com/recipes/2022/02/Trouxinha-de-repolho-com-carne-moida.png",
-      qrCode: "https://barcode-test-tan.vercel.app//serbisch/sarma"
+      qrCode: "https://barcode-test-ochre.vercel.app//serbisch/sarma"
     },
     {
       name: "Burek",
       description: "Burek ist ein serbisches Blätterteiggebäck, gefüllt mit Fleisch, Käse, Kartoffeln oder Spinat.",
       image: "https://cdn.karar.com/news/1497684.jpg",
-      qrCode: "https://barcode-test-tan.vercel.app//serbisch/burek"
+      qrCode: "https://barcode-test-ochre.vercel.app//serbisch/burek"
     },
     {
       name: "Kajmak",
       description: "Kajmak ist ein serbischer cremiger Frischkäse, der aus aufgekochter Milch hergestellt wird und oft zu Fleischgerichten serviert wird.",
       image: "https://www.thespruceeats.com/thmb/k8p2YSDzxFUTxY7yU6D-LEqgBKQ=/3200x2061/filters:fill(auto,1)/traditional-serbian-kajmak-recipe-1135791-hero-01-4cfbd4a92cbd4dce83ae0ade3d221069.jpg",
-      qrCode: "https://barcode-test-tan.vercel.app//serbisch/kajmak"
+      qrCode: "https://barcode-test-ochre.vercel.app//serbisch/kajmak"
     },
     {
       name: "Pasulj",
       description: "Pasulj ist ein serbischer Bohneneintopf, oft mit weißem Bohnen, Fleisch und Gemüse zubereitet.",
       image: "https://i.pinimg.com/originals/9a/48/d0/9a48d0f95e9107e8b021d1ef595cf6c1.jpg",
-      qrCode: "https://barcode-test-tan.vercel.app//serbisch/pasulj"
+      qrCode: "https://barcode-test-ochre.vercel.app//serbisch/pasulj"
     },
     {
       name: "Proja",
       description: "Proja ist ein serbischer Maisbrot, das mit Maismehl, Mehl, Joghurt und Käse zubereitet wird.",
       image: "https://www.kurir.rs/data/images/2017/03/25/00/1139361_seljacka-proja-recept_share.jpg",
-      qrCode: "https://barcode-test-tan.vercel.app//serbisch/proja"
+      qrCode: "https://barcode-test-ochre.vercel.app//serbisch/proja"
     },
     {
       name: "Pita",
       description: "Pita ist ein serbisches Teiggebäck, das mit verschiedenen Füllungen wie Hackfleisch, Käse oder Gemüse zubereitet wird.",
       image: "https://nomadparadise.com/wp-content/uploads/2019/11/bosnianfood_04.jpg",
-      qrCode: "https://barcode-test-tan.vercel.app//serbisch/pita"
+      qrCode: "https://barcode-test-ochre.vercel.app//serbisch/pita"
     },
     // Weitere Rezepte können hier hinzugefügt werden...
   ];
